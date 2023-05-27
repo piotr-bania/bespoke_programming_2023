19a962d1110f5713d3e10e6c7e1c08c80e940c0e
Piotr <piotr.bania@europe.com>
2023-05-28 00:32:46 +0100

    init

--- components/layout/Navbar.js
+++ components/layout/Navbar.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
     return (
         <nav>
+            <Link href="/">
+                <Image src="./svg/logo.svg" alt="logo" width={60} height={60} />
+            </Link>
+
             <li>
                 <Link href="/">Home</Link>
                 <Link href="/services">Services</Link>
