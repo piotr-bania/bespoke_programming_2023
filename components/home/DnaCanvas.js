8813aca256b21ed660aed9aa266c04951d17e1b8
Piotr <piotr.bania@europe.com>
2023-06-19 11:38:49 +0100

    init

--- components/home/DnaCanvas.js
+++ components/home/DnaCanvas.js
@@ -5,33 +5,6 @@ import { PerspectiveCamera, Environment } from '@react-three/drei'
 import Dna from './Dna'
 import Controls from '../controls/Controls'
 
-const CameraController = () => {
-    const { camera } = useThree()
-    const scrollY = useRef(window.scrollY)
-    const target = useRef({ y: 1, rotation: 0 })
-    const lerpSpeed = 0.1  // Controls how quickly the camera moves towards the target position and rotation
-
-    // Update the target position and rotation when the user scrolls
-    useEffect(() => {
-        const handleScroll = () => {
-            scrollY.current = window.scrollY
-            target.current.y = scrollY.current * 0.002  // Multiply by a small value to reduce the effect of scrolling
-            target.current.rotation = scrollY.current * 0.00025  // Rotate the camera as the user scrolls
-        };
-        window.addEventListener('scroll', handleScroll)
-        return () => window.removeEventListener('scroll', handleScroll)
-    }, [])
-
-    // Update the camera position and rotation on each frame
-    useFrame(() => {
-        camera.position.y += (target.current.y - camera.position.y) * lerpSpeed  // Lerp the y position
-        camera.rotation.y += (target.current.rotation - camera.rotation.y) * lerpSpeed  // Lerp the rotation
-        camera.updateProjectionMatrix()  // Update the camera's projection matrix after changing its position or rotation
-    })
-
-    return null
-}
-
 const DnaCanvas = () => {
 
     return (
@@ -44,9 +17,7 @@ const DnaCanvas = () => {
                 <Canvas className='canvas' shadows>
                     <Suspense fallback={null}>
                             <PerspectiveCamera makeDefault position={[0, 0, 25]} />
-                            {/* <ambientLight intensity={0.1} color={'#ffffff'} /> */}
                             <Environment files='./environment/brown_photostudio_01_1k.hdr' />
-                            {/* <pointLight color={'#FFFFFF'} position={[0, 1, 0]} intensity={0.95} /> */}
                             <Dna />
                             <Controls />
                     </Suspense>
