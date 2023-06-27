cd5d8709174768d349d7f2703bf5f5c052462097
Piotr <piotr.bania@europe.com>
2023-06-27 13:16:03 +0100

    init

--- components/home/Hero.js
+++ components/home/Hero.js
@@ -1,22 +1,29 @@
-import React, { useState, useRef, useMemo, useEffect } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { useAnimations, OutlineEffect } from '@react-three/drei'
-import { useLoader, useFrame, useThree } from '@react-three/fiber'
+import { useAnimations, useGLTF } from '@react-three/drei'
+import { useLoader, useFrame } from '@react-three/fiber'
 
 const Hero = (props) => {
 
     const globeRef = useRef()
 
-    const {scene, nodes, materials, animations} = useLoader(GLTFLoader, '/models/globe.glb')
+    const globe = useGLTF('./models/globe.gltf')
+    const animations = useAnimations(globe.animations, globe.scene)
+
+    useEffect(() => {
+        const action = animations.actions.Action
+        action.play()
+        // console.log(action)
+    }, [])
 
     useFrame((state, delta) => {
         // waterMaterial.uniforms.uTime.value += delta
-        globeRef.current.rotation.y -= 0.00025
+        globeRef.current.rotation.y -= 0.0001
     })
 
     return (
         <mesh ref={globeRef} >
-            <primitive object={scene} />
+            <primitive object={globe.scene} />
         </mesh>
     )
 }
