b7883a04dfa051e9f7d14e25e0915df8f357175b
Piotr <piotr.bania@europe.com>
2023-06-30 18:15:09 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true,  
 
   webpack: (config, options) => {
     config.module.rules.push({
