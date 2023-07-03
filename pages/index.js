470b7a110b6e575dc8b041616b0c7c023f241ccb
Piotr <piotr.bania@europe.com>
2023-07-03 16:31:57 +0100

    init

--- pages/index.js
+++ pages/index.js
@@ -12,19 +12,8 @@ export default function Home() {
                 <meta name="description" content="Bespoke Programming | Freelance Developer | London" />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/svg/logo.svg" />
-                                
-                {/* Google tag (gtag.js) */}
-                <script async src="https://www.googletagmanager.com/gtag/js?id=G-JH1ZN3CD7V"></script>
-                <script>
-                    window.dataLayer = window.dataLayer || []
-                    function gtag() {
-                        dataLayer.push(arguments)
-                    }
-                    gtag('js', new Date())
-                    gtag('config', 'G-JH1ZN3CD7V')
-                </script>
             </Head>
-
+  
             <m.main
                 initial={{opacity: 0, transition: {duration: 1}}}
                 animate={{opacity: 1, transition: {delay: 1, duration: 1}}}
