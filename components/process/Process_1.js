e09356b5e890dadcc5c8bc7d566e0dc62755bb21
Piotr <piotr.bania@europe.com>
2023-07-28 13:58:18 +0100

    init

--- components/process/Process_1.js
+++ components/process/Process_1.js
@@ -1,16 +1,18 @@
 import React, { useRef } from 'react'
-import { Center, Text } from '@react-three/drei'
+import { Float, Center, Text } from '@react-three/drei'
 
 const Process_1 = () => {
 
     return (
-        <Text
-            font='./fonts/Contralto/contraltobig-bold-webfont.woff'
-            fontSize={10}
-            color={'#6526D1'}
-        >
-            1
-        </Text>
+        <Float distance={0.5} speed={5} factor={0.5} >
+            <Text
+                font='./fonts/Contralto/contraltobig-bold-webfont.woff'
+                fontSize={10}
+                color={'#6526D1'}
+            >
+                1
+            </Text>
+        </Float>
     )
 }
 
